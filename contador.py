a5eed7915ff284b8a60ecf1cf36c7572ca4b4097
YifanZhan <90822659+YifanZhan@users.noreply.github.com>
2021-09-22 11:52:22 +0200

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,7 @@
+def contador () :
+
+    print ("voy a contar desde 0 hasta 10: ")
+
+    for cont in range (11, -1 , -5 ) :
+        print (cont)
+contador ()
---
+++ saludador2.py
@@ -0,0 +1,19 @@
+
+def saludador2 ( ) :
+
+    nombre = raw_input (" como te llamas <3 ?  ")
+
+    edad = input ("cuantos anios tienes UwU?  ")
+
+    lugar = raw_input ("donde vives 7w7 ? " )
+
+    print ("buenos dias, tarde o noche :) " + nombre )
+
+    print ("tienes  "+str (edad)+" anios ")
+
+    print (" y se que vives en 9-9  " + lugar )
+
+    print  ("ten cuidado :v ")
+
+saludador2 ( )
+           
