4b16027a4915ef3365b6af8587294f1c629b52e5
YifanZhan <90822659+YifanZhan@users.noreply.github.com>
2021-09-23 08:32:07 +0200

    Add files via upload

---
+++ tabla del 7.py
@@ -0,0 +1,9 @@
+def contador () :
+
+    print ("****------------------****")
+    print ("*       Tabla del 7         *")
+    print ("****------------------****")
+    for cont in range (0 , 11) :
+        print ( "7 x "+ str (cont) + " = " +str(7*cont))
+        print (" Ya tienes el resultado")
+contador ()
