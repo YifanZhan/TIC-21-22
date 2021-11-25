dc93ac23f81949924d6e011404111f44c713c454
YifanZhan <90822659+YifanZhan@users.noreply.github.com>
2021-11-25 08:33:32 +0100

    Add files via upload

---
+++ contador (gauss).py
@@ -0,0 +1,20 @@
+def Gauss () :
+
+    print ("contador de numeros  ")
+
+    x = input ("hasta que numero quieres que cuente?  ")
+    y = 0    
+    for cont in range (0 , x+1):
+        y=y+cont
+        print (str(cont) + " = "+ str(y))
+
+
+
+Gauss ()
+
+
+
+
+
+
+    
