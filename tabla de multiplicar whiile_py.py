7f6c254c08678ed0a2187ae0562fc39218345775
YifanZhan <90822659+YifanZhan@users.noreply.github.com>
2021-11-25 08:27:11 +0100

    Add files via upload

---
+++ tabla de multiplicar whiile_py.py
@@ -0,0 +1,9 @@
+def tabla_multiplicar_while () :
+    x = input ("dame un numero para multiplicarlo: ")
+    cont= 0
+    while ( cont<=20) :
+        print (cont*x)
+        cont= cont+1
+
+tabla_multiplicar_while ()
+
