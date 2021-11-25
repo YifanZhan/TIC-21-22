c51e7561d12f55174f2bddcfe070a0449997a777
YifanZhan <90822659+YifanZhan@users.noreply.github.com>
2021-11-25 08:26:38 +0100

    Add files via upload

---
+++ sustituye_vocales.py
@@ -0,0 +1,24 @@
+def sustituye_vocales ():
+
+    palabra=raw_input("dame una palabra ")
+    cont = 0
+    len (palabra)
+    while (cont<len(palabra)):
+        if (palabra[cont]=="a"):
+            print ("u")
+        else:
+             if (palabra[cont]=="e"):
+                      print ("u")
+             else:
+                    if (palabra[cont]=="i"):
+                        print ("u")
+                    else:
+                        if (palabra[cont]=="o"):
+                                print ("u")
+                        else:
+                                print(palabra[cont])
+        cont=cont+1
+    print ("has escrito: " + palabra)
+
+    
+sustituye_vocales ()
