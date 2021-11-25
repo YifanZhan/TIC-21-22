2aca981e70dd2afa3ddf1e60867c865b08a7d9b3
YifanZhan <90822659+YifanZhan@users.noreply.github.com>
2021-11-25 08:34:29 +0100

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+def calculadora () :
+
+    print ("*******Calculadora*******")
+
+    x= input ("dame un numero:  ")
+    y= input ("ahora dame otro numero:  ")
+
+
+    while(true):
+
+
+    print (" indique que operacion quiere hacer")
+
+           
+    
+
+
+calculadora ()
